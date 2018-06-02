3123e34f08eea5ba2ec3ef6f34f4c094ff324668
Alex Alkire <aalkire@csumb.edu>
2018-06-02 12:21:44 +0100

    foo

--- bot.js
+++ bot.js
@@ -14,10 +14,11 @@ client.on("message", (message) => {
 	  message.channel.send("Echo test.");
   }
   if (message.content.startsWith("!save")) {
-    var Attachment = (message.attachments).array();
+    var a = (message.attachments).array();
     //console.log(Attachment); //outputs array
-    if(Attachment){
-	console.log(Attachment[0]); //undefined
+    if(a){
+		fs.writeFileSync(`./${a.name}`, a.file)
+		console.log(a[0]); //undefined
     }
 	//console.log(Attachment.MessageAttachment); //undefined
     //console.log(Attachment.MessageAttachment['url']); //error
